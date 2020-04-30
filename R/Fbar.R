d5b4ef0f8f6b27fa8a82148ae394daba0378dfb4
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2020-04-30 14:31:15 +0200

    Update Fbar.R

--- R/Fbar.R
+++ R/Fbar.R
@@ -1,15 +1,3 @@
-library(Rcpp)
-
-library(RcppArmadillo)
-
-cppFunction(depends="RcppArmadillo",plugins="cpp11",code="
-            arma::vec Fbar(arma::vec& X, arma::vec& y) {
-            arma::mat M=repmat(X,1,y.n_elem);
-            arma::mat Y=repmat(trans(y),X.n_elem,1);
-            arma::vec N(y.n_elem);
-            N.ones();
-            double n=X.n_elem;
-            N=n*N;
-            return(sum(trans(M>Y),1)/N);
-            }
-            ") #quantile c.s.f.
+Fbar=function(X,y){
+  return(apply((t(matrix(rep(X,length(y)),length(X),length(y)))-matrix(rep(y,length(X)),length(y),length(X))>0),1,mean))
+}
