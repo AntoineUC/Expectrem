be9c5d48bcf8f4ae956b2e2533197ef6b0ac3234
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2020-04-30 14:17:16 +0200

    Update expect.R

--- R/expect.R
+++ R/expect.R
@@ -1,23 +1,32 @@
-library(Rcpp)
 
-library(RcppArmadillo)
-
-cppFunction(depends="RcppArmadillo",plugins="cpp11",code="
-            arma::vec expect(arma::vec& X, arma::vec& alphas) {
-            arma::mat M=repmat(X,1,alphas.n_elem);
-            arma::vec a(arma::size(alphas));
-            arma::vec b(arma::size(alphas));
-            a.zeros();
-            b.zeros();
-            arma::vec gap(arma::size(alphas));
-            gap.ones();
-            arma::mat A=repmat(trans(a),X.n_elem,1);
-            while(gap.max()>0.00001){
-              A=repmat(trans(a),X.n_elem,1);
-              b=mean(trans(M),1)-alphas%mean(trans(M-A)%trans(M<A),1)-(1-alphas)%mean(trans(M-A)%trans(M>A),1);
-              gap=abs(b-a);
-              a=b;
-            }
-            return(b);
-            }
-            ") #empirical expectile function
+expectile=function (x, probs = seq(0, 1, 0.25), dec = 4) 
+{
+  if (!is.vector(x)) 
+    stop("observations are needed in vector form.")
+  if (min(probs) < 0 || max(probs) > 1) 
+    stop("only asymmetries between 0 and 1 allowed.")
+  e = mean(x)
+  ee = 0 * probs
+  g = max(abs(x)) * 1e-06
+  for (k in 1:length(probs)) {
+    p = probs[k]
+    if (p == 0) 
+      ee[k] = min(x, na.rm = TRUE)
+    else if (p == 1) 
+      ee[k] = max(x, na.rm = TRUE)
+    else {
+      for (it in 1:20) {
+        w = ifelse(x < e, 1 - p, p)
+        enew = sum(w * x)/sum(w)
+        de = max(abs(enew - e))
+        e = enew
+        if (de < g) 
+          break
+      }
+      ee[k] = e
+    }
+  }
+  names(ee) = probs
+  ee = round(ee, dec)
+  return(ee)
+}
