a45df21e919b358a3b4e10d9b6f45ad5c49a29fe
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2020-12-18 12:37:56 +0100

    Add files via upload

--- R/epareto.R
+++ R/epareto.R
@@ -1,4 +1,4 @@
-egpd=function(probs,gamma=0.5){
+epareto=function(probs,gamma=0.5){
   
   if (min(probs) < 0 || max(probs) > 1){
     stop("only asymmetries between 0 and 1 allowed.")
@@ -10,13 +10,14 @@ egpd=function(probs,gamma=0.5){
   
   find_root=function(tau){
     fun=function(e){
-      psi1=(1+gamma*e)^(1-1/gamma)/(1-gamma)
+      psi1=-e^(1-1/gamma)/(1-1/gamma)
       return(psi1/(2*psi1+e-1/(1-gamma))-1+tau)
     } 
     
-    return(uniroot(fun,c(0,10000000))$root)
+    return(uniroot(fun,c(1,10000000))$root)
   }
   
-  return(mapply(find_root,probs))
+  mapply(find_root,probs)
   
+
 }
