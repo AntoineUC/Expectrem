36be4068a8971cbd7bb709b4528773830badb96a
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2023-02-10 14:39:20 +0100

    Update tindexp.R

--- R/tindexp.R
+++ R/tindexp.R
@@ -1,24 +1,26 @@
 tindexp=function(X,k="kopt",br=FALSE){
-
+  
   if(!is.logical(br)){
     stop("br must be boolean.")
   }
-
+  
   n=length(X)
-
+  
   mopest=mop(X[which(X>0)], 1:(length(which(X>0))-1), 0, method ="RBMOP")
-
+  
   if(k[1]=="kopt"){
     k=trunc(((1-mopest$rho)^2/(-2*mopest$rho*mopest$beta^2))^(1/(1-2*mopest$rho))*n^(-2*mopest$rho/(1-2*mopest$rho)))
     k=min(trunc(((1/mopest$EVI[k]-1)^(2*mopest$rho-1)*(1-mopest$EVI[k]-mopest$rho)^2/(-2*mopest$rho*mopest$beta^2*abs(1-2*mopest$EVI[k])))^(1/(1-2*mopest$rho))*n^(-2*mopest$rho/(1-2*mopest$rho))),trunc(n/2)-1)
   }
-
-  gammhill=mopest$EVI[k]
-
+  
+  #gammhat=mopest$EVI[k] #previous version, published in Girard, S., Stupfler, G. and Usseglio-Carleve, A., On automatic bias reduction for extreme expectile estimation, Statistics and Computing, 32(64).
+  
   qtp=expect(X,1-k/n)
-
-  r=(1-mean(X)/qtp)*(n/(n-2*k))*(1+mopest$beta*gammhill*Fbar(X,qtp)^(-mopest$rho)/(gammhill*(1-mopest$rho-gammhill)))^(-1)
-
+  
+  gammhat = 1/(1 + Fbar(X, qtp)/(k/n)) #new version (2023)
+  
+  r=(1-mean(X)/qtp)*(n/(n-2*k))*(1+mopest$beta*gammhat*Fbar(X,qtp)^(-mopest$rho)/(gammhat*(1-mopest$rho-gammhat)))^(-1)
+  
   if(br==FALSE){
     return(1/(1+Fbar(X,qtp)/(k/n)))
   }
