d693208f4a3860d541edac5c3de81e4421c3b1bd
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2023-02-21 16:54:47 +0100

    Update logspacqqplot.R

--- R/logspacqqplot.R
+++ R/logspacqqplot.R
@@ -1,7 +1,10 @@
-logspacqqplot=function(X,k=trunc(length(X)/10),grid=seq(0.01,0.99,0.01),add.line=F){
+logspacqqplot=function(X,k=trunc(length(X)/10),grid=seq(0.01,0.99,0.01),weighted=T,add.line=F){
   
   n=length(X)
   
+  if(!is.logical(weighted)){
+    stop("weighted must be boolean.")
+  }
   if(!is.logical(add.line)){
     stop("add.line must be boolean.")
   }
@@ -16,7 +19,12 @@ logspacqqplot=function(X,k=trunc(length(X)/10),grid=seq(0.01,0.99,0.01),add.line
     stop("grid must be strictly between 0 and 1")
   }
   
-  plot(y=quantile(log(quantile(X,1-1:k/n,type=1)/quantile(X,1-k/n,type=1)),grid),x=(-log(1-grid)),pch=16,main="QQ-plot",ylab="Log-spacings quantiles",xlab="Exponential quantiles")
+  if(weighted==T){
+    plot(y=quantile((1:k)*log(quantile(X,1-(1:k-1)/n,type=1)/quantile(X,1-1:k/n,type=1)),grid),x=(-log(1-grid)),pch=16,main="QQ-plot",ylab="Log-spacings quantiles",xlab="Exponential quantiles")
+  }
+  if(weighted==F){
+    plot(y=quantile(log(quantile(X,1-1:k/n,type=1)/quantile(X,1-k/n,type=1)),grid),x=(-log(1-grid)),pch=16,main="QQ-plot",ylab="Log-spacings quantiles",xlab="Exponential quantiles")
+  }
   if(add.line==T){
     abline(a=0,b=mop(X,k=k,p=0,method="RBMOP")$EVI)
   }
