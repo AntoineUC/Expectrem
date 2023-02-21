7437502029ec7f3938dd6667c7c066329d22e370
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2023-02-21 16:47:03 +0100

    Update logspaceeplot.R

--- R/logspaceeplot.R
+++ R/logspaceeplot.R
@@ -1,7 +1,10 @@
-logspaceeplot=function(X,k=trunc(length(X)/10),grid=seq(0.01,0.99,0.01),add.line=F){
+logspaceeplot=function(X,k=trunc(length(X)/10),grid=seq(0.01,0.99,0.01),weighted=T,add.line=F){
   
   n=length(X)
   
+  if(!is.logical(weighted)){
+    stop("weighted must be boolean.")
+  }
   if(!is.logical(add.line)){
     stop("add.line must be boolean.")
   }
@@ -15,8 +18,12 @@ logspaceeplot=function(X,k=trunc(length(X)/10),grid=seq(0.01,0.99,0.01),add.line
   if(any(grid>=1) || any(grid<=0)){
     stop("grid must be strictly between 0 and 1")
   }
-  
-  plot(y=expect(log(quantile(X,1-1:k/n,type=1)/quantile(X,1-k/n,type=1)),grid),x=eexp(grid),pch=16,main="EE-plot",ylab="Log-spacings expectiles",xlab="Exponential expectiles")
+  if(weighted==T){
+    plot(y=expect((1:k)*log(quantile(X,1-(1:k-1)/n,type=1)/quantile(X,1-1:k/n,type=1)),grid),x=eexp(grid),pch=16,main="EE-plot",ylab="Log-spacings expectiles",xlab="Exponential expectiles")
+  }
+  if(weighted==F){
+    plot(y=expect(log(quantile(X,1-1:k/n,type=1)/quantile(X,1-k/n,type=1)),grid),x=eexp(grid),pch=16,main="EE-plot",ylab="Log-spacings expectiles",xlab="Exponential expectiles")
+  }
   if(add.line==T){
     abline(a=0,b=mop(X,k=k,p=0,method="RBMOP")$EVI)
   }
