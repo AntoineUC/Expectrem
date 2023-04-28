d0a5297cc5974a0f08c3557712fd91bfcb5a063e
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2023-04-28 10:36:58 +0200

    Update epareto.R

--- R/epareto.R
+++ R/epareto.R
@@ -1,23 +1,18 @@
-epareto=function(probs,gamma=0.5){
-  
-  if (min(probs) < 0 || max(probs) > 1){
+epareto=function (probs, alpha=2, niter = 50) 
+{
+  if (min(probs) < 0 || max(probs) > 1) {
     stop("only asymmetries between 0 and 1 allowed.")
   }
-  
-  if(gamma<=0 || gamma>=1){
-    stop("gamma must be strictly between 0 and 1.")
+  if (alpha <= 1) {
+    stop("alpha must be greater than 1.")
   }
-  
-  find_root=function(tau){
-    fun=function(e){
-      psi1=-e^(1-1/gamma)/(1-1/gamma)
-      return(psi1/(2*psi1+e-1/(1-gamma))-1+tau)
-    } 
-    
-    return(uniroot(fun,c(1,10000000))$root)
+  e = rep(alpha/(alpha-1), length(probs))
+  i = 1
+  while (i <= niter) {
+    e = alpha/(alpha-1)*((2*probs-1)*e^(1-alpha)+1-probs)/((2*probs-1)*e^(-alpha)+1-probs)
+    i = i + 1
   }
-  
-  mapply(find_root,probs)
-  
-
+  e[which(probs == 0)] = 1
+  e[which(probs == 1)] = Inf
+  return(e)
 }
