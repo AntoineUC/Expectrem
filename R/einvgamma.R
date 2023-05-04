5f27631c6358b37553428aec1dae5e3a05a4cfdf
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2023-05-04 14:06:45 +0200

    Update einvgamma.R

--- R/einvgamma.R
+++ R/einvgamma.R
@@ -1,4 +1,4 @@
-einvgamma=function (probs, alpha = 2, lambda = 1, niter = 50) 
+einvgamma=function (probs, alpha, lambda, niter = 50) 
 {
   if (min(probs) <= 0 || max(probs) > 1) {
     stop("only asymmetries between 0 (strictly) and 1 allowed.")
@@ -9,7 +9,7 @@ einvgamma=function (probs, alpha = 2, lambda = 1, niter = 50)
   e = rep(lambda/(alpha-1), length(probs))
   i = 1
   while (i <= niter) {
-    e =(lambda*(2*probs-1)*lowincgamma(lambda/e,alpha-1)+(1-probs)*gamma(alpha)*lambda/(alpha-1))/((2*probs-1)*lowincgamma(lambda/e,alpha)+gamma(alpha)*(1-probs))
+    e = lambda/(alpha-1)*((2*probs-1)*pgamma(lambda/e,alpha-1)+1-probs)/((2*probs-1)*pgamma(lambda/e,alpha)+1-probs)
     i = i + 1
   }
   e[which(probs == 0)] = 0
