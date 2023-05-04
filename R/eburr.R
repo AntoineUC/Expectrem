1242620df2255755f60e4a0f32713eb840a18c5f
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2023-05-04 11:00:04 +0200

    Update eburr.R

--- R/eburr.R
+++ R/eburr.R
@@ -1,36 +1,25 @@
-eburr=function(probs,gamma=0.5,rho=-1){
+eburr=function(probs,a,b,niter=50){
   
   if (min(probs) < 0 || max(probs) > 1){
     stop("only asymmetries between 0 and 1 allowed.")
   }
   
-  if(gamma<=0 || gamma>=1){
-    stop("gamma must be strictly between 0 and 1.")
+  if(a<=0 || b<=0){
+    stop("Parameters must be strictly positive.")
   }
   
-  if(rho>0){
-    stop("rho must be negative.")
+  if(a*b<=1){
+    stop("Expectiles do not exist if ab is less than 1.")
   }
   
-  kmax=100
-  
-  coeffs=cumprod((1/rho-0:(kmax-1)))/factorial(1:kmax)
-  
-  find_root=function(tau){
-    fun=function(e){
-      if(e>=1){
-        psi1=-e^(1-1/gamma)/(1-1/gamma)-sum(coeffs*e^(1+(1:kmax)*rho/gamma-1/gamma)/(1+(1:kmax)*rho/gamma-1/gamma))
-      }
-      if(e<1){
-        psi1=1-e+sum(coeffs*(1-e^(1-(1:kmax)*rho/gamma))/(1-(1:kmax)*rho/gamma))-1/(1-1/gamma)-sum(coeffs/(1+(1:kmax)*rho/gamma-1/gamma))
-        
-      }
-      return(psi1/(2*psi1+e+1/rho*beta(-1/rho+gamma/rho,1-gamma/rho))-1+tau)
-    }
-    
-    return(uniroot(fun,c(0,10000000))$root)
+  e = rep(b*beta(b-1/a,1/a+1), length(probs))
+  i = 1
+  while (i <= niter) {
+    e = b*beta(b-1/a,1/a+1)*((2*probs-1)*pbeta(1/(1+e^a),b-1/a,1/a+1)+1-probs)/((2*probs-1)*(1+e^a)^(-b)+1-probs)
+    i = i + 1
   }
+  e[which(probs == 0)] = 0
+  e[which(probs == 1)] = Inf
   
-  return(mapply(find_root,probs))
-  
+  return(e)
 }
