5e840545e5d2e5d660f46233bbddb35611675d11
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2023-06-19 15:13:44 +0200

    Update egeom.R

--- R/egeom.R
+++ R/egeom.R
@@ -6,7 +6,6 @@ egeom=function (probs, p)
   if (p < 0 || p > 1) {
     stop("p must be between 0 and 1.")
   }
-  e = rep(1/p, length(probs))
   xp=1/p-1/log(1-p)*lambertWp(-(1-p)^(1/p)*log(1-p)/p*(2*probs-1)/(1-probs))
   e=((2*probs-1)*(1-p)^(trunc(xp))*(1+p*trunc(xp))+1-probs)/(p*((2*probs-1)*(1-p)^(trunc(xp))+1-probs))-1
   return(e)
