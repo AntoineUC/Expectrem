8fdc7ba9d4b4c5b873948ed6334465e091618eb9
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2023-06-19 14:18:55 +0200

    Add files via upload

---
+++ R/egeom.R
@@ -0,0 +1,13 @@
+egeom=function (probs, p, niter = 50) 
+{
+  if (min(probs) <= 0 || max(probs) >= 1) {
+    stop("only asymmetries between 0 and 1 allowed.")
+  }
+  if (p < 0 || p > 1) {
+    stop("p must be between 0 and 1.")
+  }
+  e = rep(1/p, length(probs))
+  xp=1/p-1/log(1-p)*lambertWp(-(1-p)^(1/p)*log(1-p)/p*(2*probs-1)/(1-probs))
+  e=((2*probs-1)*(1-p)^(trunc(xp))*(1+p*trunc(xp))+1-probs)/(p*((2*probs-1)*(1-p)^(trunc(xp))+1-probs))-1
+  return(e)
+}
