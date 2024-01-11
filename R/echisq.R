7b409057ed6d3f7e20af5d9af4013fafe32ae94b
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-11 13:38:23 +0100

    Update echisq.R

--- R/echisq.R
+++ R/echisq.R
@@ -1,4 +1,4 @@
-echisq=function(probs,df,tol=1e-08){
+echisq=function(probs,df,tol=1e-08,maxiter=100){
   
   if (min(probs) < 0 || max(probs) > 1){
     stop("only asymmetries between 0 and 1 allowed.")
@@ -10,14 +10,18 @@ echisq=function(probs,df,tol=1e-08){
   
   e = rep(df, length(probs))
   gap=1
-  while (gap >= tol) {
+  i=1
+  while (gap >= tol && i<= maxiter) {
     e1 = df*((2*probs-1)*(1-pgamma(e/2,df/2+1))+1-probs)/((2*probs-1)*(1-pgamma(e/2,df/2))+1-probs)
     gap=max(abs(e1-e),na.rm=T)
     e=e1
+    i=i+1
   }
   e[which(probs == 0)] = 0
   e[which(probs == 1)] = Inf
-  
+  if(i>maxiter){
+    warning("Warning: maximum of iterations reached !")
+  }
   return(e)
   
 }
