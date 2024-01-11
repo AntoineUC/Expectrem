cf6d60207004c3669ee9f4013552b33a021153ae
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-11 14:28:49 +0100

    Update elog.R

--- R/elog.R
+++ R/elog.R
@@ -14,11 +14,11 @@ elog=function(probs, mu = 0, sigma = 1, tol=1e-08, maxiter=100){
   i=1
   while(gap >= tol && i<=maxiter){
     
-    e1=(1-2*probs[which(probs*(1-probs) != 0)])*((exp(e[which(probs*(1-probs) != 0)])+1)*log(exp(e[which(probs*(1-probs) != 0)])+1)-e[which(probs*(1-probs) != 0)]*exp(e[which(probs*(1-probs) != 0)]))/((probs[which(probs*(1-probs) != 0)]-1)*exp(e[which(probs*(1-probs) != 0)])-probs[which(probs*(1-probs) != 0)])
+    e1=(1-2*probs)*((exp(e)+1)*log(exp(e)+1)-e*exp(e))/((probs-1)*exp(e)-probs)
     
-    gap=max(abs(e1-e[which(probs*(1-probs) != 0)]),na.rm=T)
+    gap=max(abs(e1-e),na.rm=T)
     
-    e[which(probs*(1-probs) != 0)]=e1
+    e=e1
 
     i=i+1
   }
