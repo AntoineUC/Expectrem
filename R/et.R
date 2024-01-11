9d642a594dd081ece0e15fe2c2e2b9bcca6b83de
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-11 14:24:49 +0100

    Update et.R

--- R/et.R
+++ R/et.R
@@ -1,6 +1,6 @@
 et=function(probs, df, mu = 0, sigma = 1, tol=1e-08, maxiter=100){
   
-  if (min(probs) < 0 || max(probs) > 1){
+  if (min(probs) <= 0 || max(probs) >= 1){
     stop("only asymmetries between 0 and 1 allowed.")
   }
   
@@ -18,18 +18,14 @@ et=function(probs, df, mu = 0, sigma = 1, tol=1e-08, maxiter=100){
   i=1
   while(gap >= tol && i<=maxiter){
     
-    e1=(2*probs[which(probs*(1-probs) != 0)]-1)*df/(1-df)*dt(e[which(probs*(1-probs) != 0)],df)*(1+e[which(probs*(1-probs) != 0)]^2/df)/((2*probs[which(probs*(1-probs) != 0)]-1)*pt(e[which(probs*(1-probs) != 0)],df)-probs[which(probs*(1-probs) != 0)])
+    e1=(2*probs-1)*df/(1-df)*dt(e,df)*(1+e^2/df)/((2*probs-1)*pt(e,df)-probs)
     
-    gap=max(abs(e1-e[which(probs*(1-probs) != 0)]),na.rm=T)
+    gap=max(abs(e1-e),na.rm=T)
     
-    e[which(probs*(1-probs) != 0)]=e1
+    e=e1
 
     i=i+1
   }
-  
-  e[which(probs==0)]=-Inf
-  
-  e[which(probs==1)]=Inf
 
   if(i>maxiter){
     warning("Warning: maximum of iterations reached !")
