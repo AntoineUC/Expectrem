eec2ae5c069f569b243037284c4e8ebde25d8b07
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-11 13:41:13 +0100

    Update efrechet.R

--- R/efrechet.R
+++ R/efrechet.R
@@ -1,4 +1,4 @@
-efrechet=function(probs,a,tol=1e-08){
+efrechet=function(probs,a,tol=1e-08,maxiter=100){
   
   if (min(probs) < 0 || max(probs) > 1){
     stop("only asymmetries between 0 and 1 allowed.")
@@ -10,13 +10,17 @@ efrechet=function(probs,a,tol=1e-08){
   
   e = rep(gamma(1-1/a), length(probs))
   gap=1
-  while (gap >= tol) {
+  i=1
+  while (gap >= tol && i<= maxiter) {
     e1 = gamma(1-1/a)*((2*probs[which(probs*(1-probs) != 0)]-1)*pgamma(e[which(probs*(1-probs) != 0)]^(-a),1-1/a)+1-probs[which(probs*(1-probs) != 0)])/((2*probs[which(probs*(1-probs) != 0)]-1)*(1-exp(-e[which(probs*(1-probs) != 0)]^(-a)))+1-probs[which(probs*(1-probs) != 0)])
     gap=max(abs(e1-e[which(probs*(1-probs) != 0)]),na.rm=T)
     e[which(probs*(1-probs) != 0)]=e1
+    i=i+1
   }
   e[which(probs == 0)] = 0
   e[which(probs == 1)] = Inf
-  
+  if(i>maxiter){
+    warning("Warning: maximum of iterations reached !")
+  }
   return(e)
 }
