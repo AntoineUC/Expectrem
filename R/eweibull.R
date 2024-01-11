cd07a5b9f143bac740aff2f6287b86dd3b49cb42
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-11 11:42:55 +0100

    Update eweibull.R

--- R/eweibull.R
+++ R/eweibull.R
@@ -1,4 +1,4 @@
-eweibull=function (probs, k, niter = 50) 
+eweibull=function (probs, k, tol=1e-08) 
 {
   if (min(probs) < 0 || max(probs) > 1) {
     stop("only asymmetries between 0 and 1 allowed.")
@@ -7,11 +7,13 @@ eweibull=function (probs, k, niter = 50)
     stop("k must be greater than 0.")
   }
   e = rep(gamma(1 + 1/k), length(probs))
-  i = 1
-  while (i <= niter) {
-    e = gamma(1 + 1/k) * ((2 * probs - 1) * (1-pgamma(e^k, 1 + 1/k)) + 1 - probs)/((2 * probs - 1) * exp(-e^k) + 
+  gap=1
+  while (gap >= tol) {
+    e1 = gamma(1 + 1/k) * ((2 * probs - 1) * (1-pgamma(e^k, 1 + 1/k)) + 1 - probs)/((2 * probs - 1) * exp(-e^k) + 
                                                                                      1 - probs)
-    i = i + 1
+    gap=max(abs(e1-e),na.rm=T)
+    
+    e=e1
   }
   e[which(probs == 0)] = 0
   e[which(probs == 1)] = Inf
