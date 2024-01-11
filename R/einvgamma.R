30b368917783fad987a4954e8f529921ef8f944f
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-11 11:55:18 +0100

    Update einvgamma.R

--- R/einvgamma.R
+++ R/einvgamma.R
@@ -1,16 +1,17 @@
-einvgamma=function (probs, alpha, lambda, niter = 50) 
+einvgamma=function (probs, alpha, lambda, tol=1e-08) 
 {
-  if (min(probs) <= 0 || max(probs) > 1) {
+  if (min(probs) < 0 || max(probs) > 1) {
     stop("only asymmetries between 0 (strictly) and 1 allowed.")
   }
   if (alpha <= 1 || lambda <= 0) {
     stop("alpha and beta must be strictly greater than 1 and 0, respectively.")
   }
   e = rep(lambda/(alpha-1), length(probs))
-  i = 1
-  while (i <= niter) {
-    e = lambda/(alpha-1)*((2*probs-1)*pgamma(lambda/e,alpha-1)+1-probs)/((2*probs-1)*pgamma(lambda/e,alpha)+1-probs)
-    i = i + 1
+  gap=1
+  while (gap >= tol) {
+    e1 = lambda/(alpha-1)*((2*probs[which(probs*(1-probs) != 0)]-1)*pgamma(lambda/e[which(probs*(1-probs) != 0)],alpha-1)+1-probs[which(probs*(1-probs) != 0)])/((2*probs[which(probs*(1-probs) != 0)]-1)*pgamma(lambda/e[which(probs*(1-probs) != 0)],alpha)+1-probs[which(probs*(1-probs) != 0)])
+    gap=max(abs(e1-e[which(probs*(1-probs) != 0)]),na.rm=T)
+    e[which(probs*(1-probs) != 0)]=e1
   }
   e[which(probs == 0)] = 0
   e[which(probs == 1)] = Inf
