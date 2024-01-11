088ec36abd1bb35dece2e46f9975fe4d8ca70e4e
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-11 13:43:30 +0100

    Update einvgamma.R

--- R/einvgamma.R
+++ R/einvgamma.R
@@ -1,4 +1,4 @@
-einvgamma=function (probs, alpha, lambda, tol=1e-08) 
+einvgamma=function (probs, alpha, lambda, tol=1e-08,maxiter=100) 
 {
   if (min(probs) < 0 || max(probs) > 1) {
     stop("only asymmetries between 0 (strictly) and 1 allowed.")
@@ -8,12 +8,17 @@ einvgamma=function (probs, alpha, lambda, tol=1e-08)
   }
   e = rep(lambda/(alpha-1), length(probs))
   gap=1
-  while (gap >= tol) {
+  i=1
+  while (gap >= tol && i<=maxiter) {
     e1 = lambda/(alpha-1)*((2*probs[which(probs*(1-probs) != 0)]-1)*pgamma(lambda/e[which(probs*(1-probs) != 0)],alpha-1)+1-probs[which(probs*(1-probs) != 0)])/((2*probs[which(probs*(1-probs) != 0)]-1)*pgamma(lambda/e[which(probs*(1-probs) != 0)],alpha)+1-probs[which(probs*(1-probs) != 0)])
     gap=max(abs(e1-e[which(probs*(1-probs) != 0)]),na.rm=T)
     e[which(probs*(1-probs) != 0)]=e1
+    i=i+1
   }
   e[which(probs == 0)] = 0
   e[which(probs == 1)] = Inf
+  if(i>maxiter){
+    warning("Warning: maximum of iterations reached !")
+  }
   return(e)
 }
