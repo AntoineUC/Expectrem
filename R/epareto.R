1a06859f8850e2a35bda3b55d4f00529942f81f4
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-11 10:40:59 +0100

    Update epareto.R

--- R/epareto.R
+++ R/epareto.R
@@ -1,4 +1,4 @@
-epareto=function (probs, alpha=2, niter = 50) 
+epareto=function (probs, alpha, tol=1e-08) 
 {
   if (min(probs) < 0 || max(probs) > 1) {
     stop("only asymmetries between 0 and 1 allowed.")
@@ -7,10 +7,11 @@ epareto=function (probs, alpha=2, niter = 50)
     stop("alpha must be greater than 1.")
   }
   e = rep(alpha/(alpha-1), length(probs))
-  i = 1
-  while (i <= niter) {
-    e = alpha/(alpha-1)*((2*probs-1)*e^(1-alpha)+1-probs)/((2*probs-1)*e^(-alpha)+1-probs)
-    i = i + 1
+  gap=1
+  while (gap >= tol) {
+    e1 = alpha/(alpha-1)*((2*probs-1)*e^(1-alpha)+1-probs)/((2*probs-1)*e^(-alpha)+1-probs)
+    gap=max(abs(e1-e),na.rm=T)
+    e=e1
   }
   e[which(probs == 0)] = 1
   e[which(probs == 1)] = Inf
