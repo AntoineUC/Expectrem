44804fbc92f66fd7d4c0e8c6e62c672c333cc724
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-11 13:37:27 +0100

    Update eburr.R

--- R/eburr.R
+++ R/eburr.R
@@ -1,4 +1,4 @@
-eburr=function(probs,a,b,tol=1e-08){
+eburr=function(probs,a,b,tol=1e-08,maxiter=100){
   
   if (min(probs) < 0 || max(probs) > 1){
     stop("only asymmetries between 0 and 1 allowed.")
@@ -14,13 +14,17 @@ eburr=function(probs,a,b,tol=1e-08){
   
   e = rep(b*beta(b-1/a,1/a+1), length(probs))
   gap=1
-  while (gap >= tol) {
+  i=1
+  while (gap >= tol && i<= maxiter) {
     e1 = b*beta(b-1/a,1/a+1)*((2*probs-1)*pbeta(1/(1+e^a),b-1/a,1/a+1)+1-probs)/((2*probs-1)*(1+e^a)^(-b)+1-probs)
     gap=max(abs(e1-e),na.rm=T)
     e=e1
+    i=i+1
   }
   e[which(probs == 0)] = 0
   e[which(probs == 1)] = Inf
-  
+  if(i>maxiter){
+    warning("Warning: maximum of iterations reached !")
+  }
   return(e)
 }
