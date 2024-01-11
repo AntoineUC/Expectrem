8b47215730c5cf82732a10d83623960a2109eed2
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-11 13:41:55 +0100

    Update egpd.R

--- R/egpd.R
+++ R/egpd.R
@@ -1,4 +1,4 @@
-egpd=function(probs,alpha,tol=1e-08){
+egpd=function(probs,alpha,tol=1e-08,maxiter=100){
   
   if (min(probs) < 0 || max(probs) > 1){
     stop("only asymmetries between 0 and 1 allowed.")
@@ -10,7 +10,8 @@ egpd=function(probs,alpha,tol=1e-08){
   
   e = rep(alpha/(alpha - 1), length(probs))
   gap=1
-  while (gap >= tol) {
+  i=1
+  while (gap >= tol && i<=maxiter) {
     e1 = alpha/(alpha - 1) * ((2 * probs - 1) * e^(1 - alpha) + 
                                1 - probs)/((2 * probs - 1) * e^(-alpha) + 1 - probs)
     gap=max(abs(e1-e),na.rm=T)
@@ -18,7 +19,9 @@ egpd=function(probs,alpha,tol=1e-08){
   }
   e[which(probs == 0)] = 1
   e[which(probs == 1)] = Inf
-  
+  if(i>maxiter){
+    warning("Warning: maximum of iterations reached !")
+  }
   return((e-1)*alpha)
   
 }
