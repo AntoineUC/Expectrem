f11e5b20d1ed1d9988161142fbe0d1ed63a9070f
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-11 13:46:34 +0100

    Update epareto.R

--- R/epareto.R
+++ R/epareto.R
@@ -1,4 +1,4 @@
-epareto=function (probs, alpha, tol=1e-08) 
+epareto=function (probs, alpha, tol=1e-08,maxiter=100) 
 {
   if (min(probs) < 0 || max(probs) > 1) {
     stop("only asymmetries between 0 and 1 allowed.")
@@ -8,12 +8,17 @@ epareto=function (probs, alpha, tol=1e-08)
   }
   e = rep(alpha/(alpha-1), length(probs))
   gap=1
-  while (gap >= tol) {
+  i=1
+  while (gap >= tol && i<=maxiter) {
     e1 = alpha/(alpha-1)*((2*probs-1)*e^(1-alpha)+1-probs)/((2*probs-1)*e^(-alpha)+1-probs)
     gap=max(abs(e1-e),na.rm=T)
     e=e1
+    i=i+1
   }
   e[which(probs == 0)] = 1
   e[which(probs == 1)] = Inf
+  if(i>maxiter){
+    warning("Warning: maximum of iterations reached !")
+  }
   return(e)
 }
