b2a81d31b5c6770d3256f836b67d9a4557bda18c
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-11 11:01:18 +0100

    Update eburr.R

--- R/eburr.R
+++ R/eburr.R
@@ -1,4 +1,4 @@
-eburr=function(probs,a,b,niter=50){
+eburr=function(probs,a,b,tol=1e-08){
   
   if (min(probs) < 0 || max(probs) > 1){
     stop("only asymmetries between 0 and 1 allowed.")
@@ -13,10 +13,11 @@ eburr=function(probs,a,b,niter=50){
   }
   
   e = rep(b*beta(b-1/a,1/a+1), length(probs))
-  i = 1
-  while (i <= niter) {
-    e = b*beta(b-1/a,1/a+1)*((2*probs-1)*pbeta(1/(1+e^a),b-1/a,1/a+1)+1-probs)/((2*probs-1)*(1+e^a)^(-b)+1-probs)
-    i = i + 1
+  gap=1
+  while (gap >= tol) {
+    e1 = b*beta(b-1/a,1/a+1)*((2*probs-1)*pbeta(1/(1+e^a),b-1/a,1/a+1)+1-probs)/((2*probs-1)*(1+e^a)^(-b)+1-probs)
+    gap=max(abs(e1-e),na.rm=T)
+    e=e1
   }
   e[which(probs == 0)] = 0
   e[which(probs == 1)] = Inf
