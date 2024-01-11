6495c59ced9c72ecccf1da6edb3b7461a7fefb20
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-11 14:17:37 +0100

    Update ebeta.R

--- R/ebeta.R
+++ R/ebeta.R
@@ -1,6 +1,6 @@
 ebeta=function (probs, shape1, shape2, tol=1e-08, maxiter=100) 
 {
-  if (min(probs) < 0 || max(probs) > 1) {
+  if (min(probs) <= 0 || max(probs) >= 1) {
     stop("only asymmetries between 0 and 1 allowed.")
   }
   if (shape1 <= 0 || shape2 <=0) {
@@ -15,8 +15,6 @@ ebeta=function (probs, shape1, shape2, tol=1e-08, maxiter=100)
     e=e1
     i=i+1
   }
-  e[which(probs == 0)] = 0
-  e[which(probs == 1)] = 1
   if(i>maxiter){
     warning("Warning: maximum of iterations reached !")
   }
