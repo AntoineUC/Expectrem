7636f2df8ada98498f7f668c6d6185d1202ac4ac
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-11 10:55:52 +0100

    Update ebeta.R

--- R/ebeta.R
+++ R/ebeta.R
@@ -1,4 +1,4 @@
-ebeta=function (probs, shape1, shape2, niter = 50) 
+ebeta=function (probs, shape1, shape2, tol=1e-08) 
 {
   if (min(probs) < 0 || max(probs) > 1) {
     stop("only asymmetries between 0 and 1 allowed.")
@@ -7,10 +7,11 @@ ebeta=function (probs, shape1, shape2, niter = 50)
     stop("shape parameters must be strictly positive.")
   }
   e = rep(shape1/(shape1+shape2), length(probs))
-  i = 1
-  while (i <= niter) {
-    e = shape1/(shape1+shape2)*((2*probs-1)*(1-pbeta(e,shape1+1,shape2))+1-probs)/((2*probs-1)*(1-pbeta(e,shape1,shape2))+1-probs)
-    i = i + 1
+  gap=1
+  while (gap >= tol) {
+    e1 = shape1/(shape1+shape2)*((2*probs-1)*(1-pbeta(e,shape1+1,shape2))+1-probs)/((2*probs-1)*(1-pbeta(e,shape1,shape2))+1-probs)
+    gap=max(abs(e1-e),na.rm=T)
+    e=e1
   }
   e[which(probs == 0)] = 0
   e[which(probs == 1)] = 1
