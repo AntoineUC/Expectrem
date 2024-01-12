c5220189985a07174f55868637758fa9a66a9180
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-12 16:11:41 +0100

    Update efrechet.R

--- R/efrechet.R
+++ R/efrechet.R
@@ -1,4 +1,4 @@
-efrechet=function(probs,a,tol=1e-08,maxiter=100){
+efrechet=function(probs, a, start.pt=log(1/probs)^(-1/a), tol=1e-08, maxiter=100){
   
   if (min(probs) <= 0 || max(probs) >= 1){
     stop("only asymmetries between 0 and 1 allowed.")
@@ -8,7 +8,11 @@ efrechet=function(probs,a,tol=1e-08,maxiter=100){
     stop("Expectiles do not exist if a is less than 1.")
   }
   
-  e = rep(gamma(1-1/a), length(probs))
+  if (length(probs) != length(start.pt)) {
+    stop("probs and start.pt must have the same length.")
+  }
+  
+  e = start.pt
   gap=1
   i=1
   while (gap >= tol && i<= maxiter) {
