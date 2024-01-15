d8a0277d3f3d7eac671c2b5167d09730b4027544
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-15 10:37:39 +0100

    Update ehallweiss.R

--- R/ehallweiss.R
+++ R/ehallweiss.R
@@ -1,11 +1,32 @@
-ehallweiss=function (probs, alpha, beta, tol=1e-08, maxiter=100) {
+ehallweiss=function (probs, alpha, beta, start.pt="quantile", tol=1e-08, maxiter=100, x0=NULL) {
+  
   if (min(probs) <= 0 || max(probs) >= 1) {
     stop("only asymmetries between 0 and 1 allowed.")
   }
+  
   if (alpha <= 1 || beta<=0) {
     stop("alpha and beta must be greater than 1 and 0, respectively.")
   }
-  e = rep((2*(alpha-1)*(alpha+beta)+beta)/(2*(alpha-1)*(alpha+beta-1)), length(probs))
+  
+  if (start.pt != "mean" && start.pt != "quantile" && start.pt != "custom") {
+    stop("start.pt may be either custom, mean or quantile.")
+  }
+  
+  if(start.pt=="mean"){
+    e=rep((2*(alpha-1)*(alpha+beta)+beta)/(2*(alpha-1)*(alpha+beta-1)), length(probs))
+  }
+  
+  if(start.pt=="quantile"){
+    e=(2*(1-probs))^(-1/alpha)
+  }
+  
+  if(start.pt=="custom"){
+    if(length(x0) != length(probs)){
+      stop("x0 and probs must have the same length.")
+    }
+    e=x0
+  }
+  
   gap=1
   i=1
   while (gap >= tol && i<=maxiter) {
