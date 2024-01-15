8e98b01192b44add28242568d2b6dad82e06ab79
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-15 10:23:03 +0100

    Update efrechet.R

--- R/efrechet.R
+++ R/efrechet.R
@@ -1,4 +1,4 @@
-efrechet=function(probs, a, start.pt=log(1/probs)^(-1/a), tol=1e-08, maxiter=100){
+efrechet=function(probs, a, start.pt="quantile", tol=1e-08, maxiter=100, x0=NULL){
   
   if (min(probs) <= 0 || max(probs) >= 1){
     stop("only asymmetries between 0 and 1 allowed.")
@@ -8,11 +8,25 @@ efrechet=function(probs, a, start.pt=log(1/probs)^(-1/a), tol=1e-08, maxiter=100
     stop("Expectiles do not exist if a is less than 1.")
   }
   
-  if (length(probs) != length(start.pt)) {
-    stop("probs and start.pt must have the same length.")
+  if (start.pt != "mean" && start.pt != "quantile" && start.pt != "custom") {
+    stop("start.pt may be either custom, mean or quantile.")
   }
   
-  e = start.pt
+  if(start.pt=="mean"){
+    e=rep(gamma(1-1/a), length(probs))
+  }
+  
+  if(start.pt=="quantile"){
+    e=log(1/probs)^(-1/a)
+  }
+  
+  if(start.pt=="custom"){
+    if(length(x0) != length(probs)){
+      stop("x0 and probs must have the same length.")
+    }
+    e=x0
+  }
+  
   gap=1
   i=1
   while (gap >= tol && i<= maxiter) {
