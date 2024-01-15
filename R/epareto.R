b842fd2910510c596a6d3322bc52157bcb8566ad
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-15 11:32:14 +0100

    Update epareto.R

--- R/epareto.R
+++ R/epareto.R
@@ -1,12 +1,33 @@
-epareto=function (probs, alpha, tol=1e-08, maxiter=100) 
+epareto=function (probs, alpha, start.pt="quantile", tol=1e-08, maxiter=100, x0=NULL) 
 {
+  
   if (min(probs) <= 0 || max(probs) >= 1) {
     stop("only asymmetries between 0 and 1 allowed.")
   }
+  
   if (alpha <= 1) {
     stop("alpha must be greater than 1.")
   }
-  e = rep(alpha/(alpha-1), length(probs))
+  
+  if (start.pt != "mean" && start.pt != "quantile" && start.pt != "custom") {
+    stop("start.pt may be either custom, mean or quantile.")
+  }
+  
+  if(start.pt=="mean"){
+    e=rep(alpha/(alpha-1), length(probs))
+  }
+  
+  if(start.pt=="quantile"){
+    e=(1-probs)^(-1/alpha)
+  }
+  
+  if(start.pt=="custom"){
+    if(length(x0) != length(probs)){
+      stop("x0 and probs must have the same length.")
+    }
+    e=x0
+  }
+  
   gap=1
   i=1
   while (gap >= tol && i<=maxiter) {
