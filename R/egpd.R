46c8de6312e9a6e2742cf24f2124b6bea3849618
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-01-15 10:31:42 +0100

    Update egpd.R

--- R/egpd.R
+++ R/egpd.R
@@ -1,4 +1,4 @@
-egpd=function(probs,alpha,tol=1e-08,maxiter=100){
+egpd=function(probs, alpha, start.pt="quantile", tol=1e-08, maxiter=100, x0=NULL){
   
   if (min(probs) <= 0 || max(probs) >= 1){
     stop("only asymmetries between 0 and 1 allowed.")
@@ -8,12 +8,30 @@ egpd=function(probs,alpha,tol=1e-08,maxiter=100){
     stop("alpha must be strictly greater than 1.")
   }
   
-  e = rep(alpha/(alpha - 1), length(probs))
+  if (start.pt != "mean" && start.pt != "quantile" && start.pt != "custom") {
+    stop("start.pt may be either custom, mean or quantile.")
+  }
+  
+  if(start.pt=="mean"){
+    e=rep(alpha/(alpha - 1), length(probs))
+  }
+  
+  if(start.pt=="quantile"){
+    e=((1-probs)^(-1/alpha)-1)*alpha
+  }
+  
+  if(start.pt=="custom"){
+    if(length(x0) != length(probs)){
+      stop("x0 and probs must have the same length.")
+    }
+    e=x0
+  }
+  
   gap=1
   i=1
   while (gap >= tol && i<=maxiter) {
     e1 = alpha/(alpha - 1) * ((2 * probs - 1) * e^(1 - alpha) + 
-                               1 - probs)/((2 * probs - 1) * e^(-alpha) + 1 - probs)
+                                1 - probs)/((2 * probs - 1) * e^(-alpha) + 1 - probs)
     gap=max(abs(e1-e),na.rm=T)
     e=e1
     i=i+1
