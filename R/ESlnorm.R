7a7c2f35b99b2a5b9dcd81104ceb4d62314c3806
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-05-17 10:23:15 +0200

    Add files via upload

---
+++ R/ESlnorm.R
@@ -0,0 +1,22 @@
+ESlnorm=function (probs, mu = 0, sigma = 1) 
+{
+  
+  if (min(probs) <= 0 || max(probs) >= 1) {
+    stop("only asymmetries between 0 and 1 allowed.")
+  }
+  
+  if (length(mu) > 1) {
+    stop("mu must be of length 1.")
+  }
+  
+  if (length(sigma) > 1) {
+    stop("sigma must be of length 1.")
+  }
+  
+  if (sigma <= 0) {
+    stop("sigma must be positive.")
+  }
+
+  return(exp(mu+sigma^2/2)*pnorm(sigma-qnorm(probs))/(1-probs))
+
+}
