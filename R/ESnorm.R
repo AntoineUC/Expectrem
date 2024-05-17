6de0de0e205ce929657c9404b292d7140e4bc48f
AntoineUC <56665669+AntoineUC@users.noreply.github.com>
2024-05-17 09:49:05 +0200

    Add files via upload

---
+++ R/ESnorm.R
@@ -0,0 +1,20 @@
+ESnorm=function(probs, mu = 0, sigma = 1){
+  
+  if (min(probs) <= 0 || max(probs) >= 1){
+    stop("only asymmetries between 0 and 1 allowed.")
+  }
+  
+  if (length(mu) > 1) {
+    stop("mu must be of length 1.")
+  }
+  
+  if (length(sigma) > 1) {
+    stop("sigma must be of length 1.")
+  }
+  
+  if(sigma<0){
+    stop("sigma must be positive.")
+  }
+  
+  return(mu+sigma*dnorm(qnorm(probs))/(1-probs))
+}
